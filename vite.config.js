e55079b7febe4000110c6ad9039360ab5037fc5d
Abhishek Vinod <59474777+AbhishekVinod-dev@users.noreply.github.com>
2026-02-11 20:04:41 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ plugins: [react()], base: '/CodeKrafters-web/' })
+export default defineConfig({ plugins: [react()], base: '/Codekrafters-web/' })
