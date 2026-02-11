c712d9d30472b09c3f26fbd98551779a1d22700a
Abhishek Vinod <59474777+AbhishekVinod-dev@users.noreply.github.com>
2026-02-11 20:35:21 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { ThemeProvider } from "./context/ThemeContext"
@@ -15,6 +16,31 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <BrowserRouter>
           <App />
         </BrowserRouter>
+      </SidebarProvider> 
+      </ThemeProvider>
+     
+  </React.StrictMode>
+)
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
+import { ThemeProvider } from "./context/ThemeContext"
+import { SidebarProvider } from "./context/SidebarContext"
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+      <ThemeProvider> 
+      <SidebarProvider>
+        <HashRouter>
+          <App />
+        </HashRouter>
       </SidebarProvider> 
       </ThemeProvider>
      
